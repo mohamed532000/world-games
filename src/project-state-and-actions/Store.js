fdf4cce8590fba2ad64ef2ac46323ea2eb89fd87
Mohamed Ezat <ezat6518@gmail.com>
2026-02-25 07:32:09 +0200

    remove auth reducer

--- src/project-state-and-actions/Store.js
+++ src/project-state-and-actions/Store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AddToCartWorker from "./AddToCartActions";
 import GamesReducer from "./GamesReducer";
-// import UserAuthWorker from "./AuthReducer";
 
 export let myStore = configureStore({
     reducer : {
         GamesReducer,
         AddToCartWorker,
-        // UserAuthWorker
     }
 });
